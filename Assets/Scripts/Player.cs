d44f81b4e152d3521e85facd94aadef1c5f67619
KangggX <lowkangxuan@gmail.com>
2021-07-23 17:32:16 +0800

    initial commit
    first ever commit to initialize repo

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// The distance this player will travel per second.
+    /// </summary>
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    /// <summary>
+    /// The camera attached to the player model.
+    /// Should be dragged in from Inspector.
+    /// </summary>
+    public Camera playerCamera;
+
+    private string currentState;
+    private string nextState;
+    private float storedRotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        storedRotationSpeed = rotationSpeed;
+        nextState = "Idle";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextState != currentState)
+        {
+            SwitchState();
+        }
+
+        CheckRotation();
+    }
+
+    /// <summary>
+    /// Sets the current state of the player
+    /// and starts the correct coroutine.
+    /// </summary>
+    private void SwitchState()
+    {
+        StopCoroutine(currentState);
+
+        currentState = nextState;
+        StartCoroutine(currentState);
+    }
+
+    private IEnumerator Idle()
+    {
+        while (currentState == "Idle")
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                nextState = "Moving";
+            }
+            yield return null;
+        }
+    }
+
+    private IEnumerator Moving()
+    {
+        while (currentState == "Moving")
+        {
+            if (!CheckMovement())
+            {
+                nextState = "Idle";
+            }
+            yield return null;
+        }
+    }
+
+    private void CheckRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+
+        Vector3 playerRotation = transform.rotation.eulerAngles;
+        playerRotation.y += mouseX;
+
+        transform.rotation = Quaternion.Euler(playerRotation);
+
+        Vector3 cameraRotation = playerCamera.transform.rotation.eulerAngles;
+        cameraRotation.x -= mouseY;
+
+        playerCamera.transform.rotation = Quaternion.Euler(cameraRotation);
+    }
+
+    /// <summary>
+    /// Checks and handles movement of the player
+    /// </summary>
+    /// <returns>True if user input is detected and player is moved.</returns>
+    private bool CheckMovement()
+    {
+        Vector3 newPos = transform.position;
+
+        Vector3 xMovement = transform.right * Input.GetAxis("Horizontal");
+        Vector3 zMovement = transform.forward * Input.GetAxis("Vertical");
+
+        Vector3 movementVector = xMovement + zMovement;
+        //Debug.Log(movementVector);
+        //Vector3 movementVector = xMovement;
+
+        if (movementVector.sqrMagnitude > 0)
+        {
+            movementVector *= moveSpeed * Time.deltaTime;
+            newPos += movementVector;
+
+            transform.position = newPos;
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+}
