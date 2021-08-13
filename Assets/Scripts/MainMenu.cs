f30e187439d8208ea0f7ec813188663d0ac01d66
rebeccalim20 <s10207898@connect.np.edu.sg>
2021-08-13 23:07:17 +0800

    added main menu audio and scripting

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+     AudioSource audio;
+    public AudioClip Hover;
+    public AudioClip click;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        
+    }
+    public void hoversound()
+    {
+        audio.PlayOneShot(Hover);
+    }
+    public void clicksound()
+    {
+        audio.PlayOneShot(click);
+    }
     public void StartGame()
     {
         //Debug.Log("Game is running");
